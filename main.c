0506ff2cc00bf36cb5b1f15b8a400dc79f1524c8
RoniEmad <roni.emad1@gmail.com>
2023-10-27 19:18:53 +0200

    add-feature-getAverage

--- main.c
+++ main.c
@@ -8,7 +8,13 @@ int getSum(vector<int>& nums)
     return accumulate(nums.begin(),nums.end(),0);
 }
 
+int getAverage(vector<int>& nums) 
+{
+    return accumulate(nums.begin(),nums.end(),0)/nums.size();
+}
+
 int main() {
     vector<int> identical {42,53,62,1,214,65,25,7,86,5};
     cout << "Sum: " << getSum(identical) << endl;
+    cout << "Average: " << getAverage(identical) << endl;
 }
