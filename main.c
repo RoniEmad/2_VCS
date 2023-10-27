4cfcb459bd656d39a8786d14b228a927684f6959
RoniEmad <roni.emad1@gmail.com>
2023-10-27 19:21:41 +0200

    add-feature-getMin

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<numeric>
+#include<algorithm>
 using namespace std;
 
 int getSum(vector<int>& nums) 
@@ -8,7 +9,13 @@ int getSum(vector<int>& nums)
     return accumulate(nums.begin(),nums.end(),0);
 }
 
+int getMin(vector<int>& nums) 
+{
+    return *min_element(nums.begin(),nums.end());
+}
+
 int main() {
     vector<int> identical {42,53,62,1,214,65,25,7,86,5};
     cout << "Sum: " << getSum(identical) << endl;
+    cout << "Min: " << getMin(identical) << endl;
 }
